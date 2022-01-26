581f9f0ff8649c598a8e7c98cf644ea1b8864416
Xok <xok_da@mail.ru>
2022-01-26 15:42:14 +0500

    Как мы обрабатываем - готово

---
+++ js/script.js
@@ -0,0 +1,8 @@
+"use strict";
+// step-item.onclick=function() {
+//     const hideText = document.getElementById('hide-text');
+//     hideText.style.display = 'block';
+// }; 
+// color.onclick = function() {
+//     this.style.backgroundColor = 'red'; 
+// };
