95bce602e0b5d4496b2e874cdaeab092ef93a058
Xok <xok_da@mail.ru>
2022-01-28 09:28:08 +0500

    Главная таблет - в процессе

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
 "use strict";
-// step-item.onclick=function() {
-//     const hideText = document.getElementById('hide-text');
-//     hideText.style.display = 'block';
-// }; 
-// color.onclick = function() {
-//     this.style.backgroundColor = 'red'; 
-// };
+// /* Открыть */
+// function openNav() {
+//     document.getElementById("myNav").style.height = "400px";
+// }
+  
+// /* Закрыть */
+// function closeNav() {
+//     document.getElementById("myNav").style.height = "0%";
+// }
+// var menuLink = document.getElementsByClassName('menu__link');
+// menuLink.onclick = function(event) {
+//     document.getElementById("myNav").classList.toggle("show");
+// }
+
+// /* Когда пользователь нажимает на кнопку,
+// переключение между скрытием и отображением раскрывающегося содержимого */
+function openMenu() {
+    document.getElementById("myNav").classList.toggle("show");
+    // document.getElementById("overlay").classList.toggle("show");
+}
+  
+//   // Закройте выпадающее меню, если пользователь щелкает за его пределами
+// window.onclick = function(event) {
+// if (!event.target.matches('.menu')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//     var openDropdown = dropdowns[i];
+//     if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//     }
+//     }
+// }
+// }
