6f29d96f0e4925c7daa5b7d71ad8fd648adc8720
Xok <xok_da@mail.ru>
2022-02-04 10:08:41 +0500

    слияние правок_3

--- js/script.js
+++ js/script.js
@@ -1,36 +1,34 @@
 $(document).ready(function(){
-    if ($(window).width()<991) {
-        $('.menu__btn').click(function(e) {
-            e.preventDefault();
-            if ($(this).hasClass('active')) {
-            $(this).next('.menu').slideUp(500);
-            } else {
-            $(this).next('.menu').slideDown(500);
-            }
-            $(this).toggleClass('active');
-            $('body').toggleClass('body-active');
-        });
-        $('.overlay').click(function(e) {
-            e.preventDefault();
-            $('body').removeClass('body-active');
-            $('.menu__btn').removeClass('active');
-            $(this).closest('body').find('.menu').slideUp(300);
-        });
-        $('.menu__link').click(function(e) {
-            e.preventDefault();
-            $('body').removeClass('body-active');
-            $('.menu__btn').removeClass('active');
-            $(this).closest('body').find('.menu').slideUp(300);
-        });
+    $('.menu__btn').click(function(e) {
+        e.preventDefault();
+        if ($(this).hasClass('active')) {
+        $(this).next('.menu').slideUp(500);
+        } else {
+        $(this).next('.menu').slideDown(500);
+        }
+        $(this).toggleClass('active');
+        $('body').toggleClass('body-active');
+    });
+    $('.overlay').click(function(e) {
+		e.preventDefault();
+		$('body').removeClass('body-active');
+		$('.menu__btn').removeClass('active');
+        $(this).closest('body').find('.menu').slideUp(300);
+	});
+    $('.menu__link').click(function(e) {
+		e.preventDefault();
+		$('body').removeClass('body-active');
+		$('.menu__btn').removeClass('active');
+        $(this).closest('body').find('.menu').slideUp(300);
+	});
 
-        $('.proces-step__list__item').click(function(e) {
-            e.preventDefault();
-            if ($(this).hasClass('active')) {
-            $(this).find('.proces-step__list__item__hide-text').slideUp(500);
-            } else {
-            $(this).find('.proces-step__list__item__hide-text').slideDown(500);
-            }
-            $(this).toggleClass('active');
-        });
-    }
+    $('.proces-step__list__item').click(function(e) {
+        e.preventDefault();
+        if ($(this).hasClass('active')) {
+        $(this).find('.proces-step__list__item__hide-text').slideUp(500);
+        } else {
+        $(this).find('.proces-step__list__item__hide-text').slideDown(500);
+        }
+        $(this).toggleClass('active');
+    });
 });
